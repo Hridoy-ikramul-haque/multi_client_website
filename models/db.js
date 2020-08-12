5554ef81cb0f0add999e8a7db2fbb3bd55f550bc
IKRAMUL HAQUE HRIDOY <mdikramulhaquehridoy@gmail.com>
2020-08-12 20:24:04 +0600

    database

---
+++ models/db.js
@@ -0,0 +1,60 @@
+var mysql=require('mysql');
+
+
+
+function getConnection(callback){
+    var con=mysql.createConnection({
+        host	: 'localhost',
+        user	: 'root',
+        password: '',
+        database: 'mmulti_client_website'
+    });
+
+    //asyncronous
+    con.connect(function(err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+        console.log('connected as id ' + con.threadId);
+        callback(con);
+    });
+}
+
+
+module.exports={
+    getResults:function (sql, callback){
+        getConnection(function (con){
+            con.query(sql, function(error, results){
+                if(error){
+                    console.log(error.stack);
+                    callback([]);
+                }else{
+                    callback(results);
+                }
+            });
+            con.end(function(err){
+                console.log('connection end...');
+            });
+        });
+
+    },
+    execute: function (sql, callback){
+
+        getConnection(function(connection){
+            connection.query(sql, function(error, results){
+
+                if(error){
+                    console.log(error.stack);
+                    callback(false);
+                }else{
+                    callback(true);
+                }
+            });
+
+            connection.end(function(err){
+                console.log('connection end...');
+            });
+        });
+    }
+}
